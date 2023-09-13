e7167e2ba433372567df56b10c2dc8bd094d3835
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-09-12 21:44:27 -0400

    test[frequency-counters-and-multiple-pointers]: download Springboard-provided tests

--- divide_and_conquer_exercises.py
+++ divide_and_conquer_exercises.py
@@ -47,3 +47,11 @@ def findLastIndex(arr, target):
         elif arr[middleIdx] > target:
             rightIdx = middleIdx - 1
     return rightIdx if rightIdx >= 0 and arr[rightIdx] == target else -1
+
+def findRotatedIndex(arr, target):
+    """
+    Write a function called findRotatedIndex
+    which accepts a rotated array of sorted numbers and an integer.
+    The function should return the index of num in the array. If the value is not found, return -1.
+    """
+    return 'something'
