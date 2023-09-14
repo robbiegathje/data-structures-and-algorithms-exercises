335532e701750cb32b074def6ddc30ba281f1dfc
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-09-13 22:15:08 -0400

    [frequency-counters-and-multiple-pointers]: complete separatePositive problem solving challenge

--- frequency-counters-and-multiple-pointers/separate-positive.js
+++ frequency-counters-and-multiple-pointers/separate-positive.js
@@ -2,4 +2,17 @@
 // Separate the positive integers to the left and the negative integers to the right.
 // The positive numbers and negative numbers need not be in sorted order.
 // The problem should be done in place (in other words, do not build a copy of the input array).
-function separatePositive(integers) {}
+function separatePositive(integers) {
+	let positiveIndex = 0;
+	let negativeIndex = integers.length - 1;
+	while (positiveIndex < negativeIndex) {
+		if (integers[positiveIndex] > 0) {
+			positiveIndex++;
+		} else if (integers[positiveIndex] < 0) {
+			integers.push(integers[positiveIndex]);
+			integers.splice(positiveIndex, 1);
+			negativeIndex--;
+		}
+	}
+	return integers;
+}
