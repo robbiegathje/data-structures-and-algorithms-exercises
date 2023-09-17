e6ed4936ba1fdd3a37e4f443f2d9dd9b5ed5838e
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-09-17 14:29:12 -0400

    [stacks-and-queues]: implement Stack and Queue data structure classes with appropriate methods

--- stacks-and-queues/queue.js
+++ stacks-and-queues/queue.js
@@ -1,46 +1,64 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
 
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+	/** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+	enqueue(val) {
+		const node = new Node(val);
+		if (this.size === 0) {
+			this.first = node;
+			this.last = node;
+		} else {
+			this.last.next = node;
+			this.last = node;
+		}
+		this.size++;
+	}
 
-  }
+	/** dequeue(): remove the node from the start of the queue
+	 * and return its value. Should throw an error if the queue is empty. */
 
-  /** dequeue(): remove the node from the start of the queue
-   * and return its value. Should throw an error if the queue is empty. */
+	dequeue() {
+		const output = this.first.val;
+		if (this.size === 0) {
+			throw Error('Queue is empty.');
+		} else if (this.size === 1) {
+			this.first = null;
+			this.last = null;
+		} else {
+			this.first = this.first.next;
+		}
+		this.size--;
+		return output;
+	}
 
-  dequeue() {
+	/** peek(): return the value of the first node in the queue. */
 
-  }
+	peek() {
+		return this.first.val;
+	}
 
-  /** peek(): return the value of the first node in the queue. */
+	/** isEmpty(): return true if the queue is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the queue is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {
+		return this.size === 0;
+	}
 }
 
 module.exports = Queue;
--- stacks-and-queues/stack.js
+++ stacks-and-queues/stack.js
@@ -1,46 +1,65 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {
+		const node = new Node(val);
+		if (this.size === 0) {
+			this.first = node;
+			this.last = node;
+		} else {
+			const currentFirst = this.first;
+			this.first = node;
+			this.first.next = currentFirst;
+		}
+		this.size++;
+	}
 
-  }
+	/** pop(): remove the node from the top of the stack
+	 * and return its value. Should throw an error if the stack is empty. */
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+	pop() {
+		const output = this.first.val;
+		if (this.size === 0) {
+			throw Error('Stack is empty.');
+		} else if (this.size === 1) {
+			this.first = null;
+			this.last = null;
+		} else {
+			this.first = this.first.next;
+		}
+		this.size--;
+		return output;
+	}
 
-  pop() {
+	/** peek(): return the value of the first node in the stack. */
 
-  }
+	peek() {
+		return this.first.val;
+	}
 
-  /** peek(): return the value of the first node in the stack. */
+	/** isEmpty(): return true if the stack is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {
+		return this.size === 0;
+	}
 }
 
 module.exports = Stack;
