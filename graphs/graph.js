8200c3112ebc9fc1058615440c9bb02dae74f4ea
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-10-01 22:26:53 -0400

    [graphs]: write all required methods for Graph class; all tests passing

--- graphs/graph.js
+++ graphs/graph.js
@@ -1,35 +1,88 @@
 class Node {
-  constructor(value, adjacent = new Set()) {
-    this.value = value;
-    this.adjacent = adjacent;
-  }
+	constructor(value, adjacent = new Set()) {
+		this.value = value;
+		this.adjacent = adjacent;
+	}
 }
 
 class Graph {
-  constructor() {
-    this.nodes = new Set();
-  }
+	constructor() {
+		this.nodes = new Set();
+	}
 
-  // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+	// this function accepts a Node instance and adds it to the nodes property on the graph
+	addVertex(vertex) {
+		this.nodes.add(vertex);
+	}
 
-  // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+	// this function accepts an array of Node instances and adds them to the nodes property on the graph
+	addVertices(vertexArray) {
+		for (let vertex of vertexArray) {
+			this.addVertex(vertex);
+		}
+	}
 
-  // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+	// this function accepts two vertices and updates their adjacent values to include the other vertex
+	addEdge(firstVertex, secondVertex) {
+		firstVertex.adjacent.add(secondVertex);
+		secondVertex.adjacent.add(firstVertex);
+	}
 
-  // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+	// this function accepts two vertices and updates their adjacent values to remove the other vertex
+	removeEdge(firstVertex, secondVertex) {
+		firstVertex.adjacent.delete(secondVertex);
+		secondVertex.adjacent.delete(firstVertex);
+	}
 
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+	// this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+	removeVertex(vertex) {
+		vertex.adjacent.forEach((adjVertex) => {
+			this.removeEdge(vertex, adjVertex);
+		});
+		this.nodes.delete(vertex);
+	}
 
-  // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+	// this function returns an array of Node values using DFS
+	depthFirstSearch(start) {
+		let toVisitStack = [start];
+		let haveSeen = new Set(toVisitStack);
+		let output = [];
 
-  // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+		while (toVisitStack.length > 0) {
+			let current = toVisitStack.pop();
+			output.push(current.value);
+			for (let vertex of current.adjacent) {
+				if (haveSeen.has(vertex)) {
+					continue;
+				} else {
+					toVisitStack.push(vertex);
+					haveSeen.add(vertex);
+				}
+			}
+		}
+		return output;
+	}
+
+	// this function returns an array of Node values using BFS
+	breadthFirstSearch(start) {
+		let toVisitStack = [start];
+		let haveSeen = new Set(toVisitStack);
+		let output = [];
+
+		while (toVisitStack.length > 0) {
+			let current = toVisitStack.shift();
+			output.push(current.value);
+			for (let vertex of current.adjacent) {
+				if (haveSeen.has(vertex)) {
+					continue;
+				} else {
+					toVisitStack.push(vertex);
+					haveSeen.add(vertex);
+				}
+			}
+		}
+		return output;
+	}
 }
 
-module.exports = {Graph, Node}
+module.exports = { Graph, Node };
