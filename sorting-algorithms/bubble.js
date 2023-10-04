b3fc5ff3be018bc9627227ac03092b1e5b3d799f
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-10-04 15:52:09 -0400

    [sorting-algorithms]: implement bubbleSort, merge, and mergeSort functions; all related tests passing

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -1,3 +1,14 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+	for (let i = nums.length - 1; i > 0; i--) {
+		for (let j = 0; j < i; j++) {
+			if (nums[j] > nums[j + 1]) {
+				[nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+			} else {
+				continue;
+			}
+		}
+	}
+	return nums;
+}
 
 module.exports = bubbleSort;
--- sorting-algorithms/merge.js
+++ sorting-algorithms/merge.js
@@ -1,5 +1,38 @@
-function merge() {}
+function merge(nums, comparison) {
+	let numsPointer = 0;
+	let comparisonPointer = 0;
+	let output = [];
 
-function mergeSort() {}
+	while (numsPointer < nums.length && comparisonPointer < comparison.length) {
+		if (nums[numsPointer] < comparison[comparisonPointer]) {
+			output.push(nums[numsPointer]);
+			numsPointer++;
+		} else {
+			output.push(comparison[comparisonPointer]);
+			comparisonPointer++;
+		}
+	}
 
-module.exports = { merge, mergeSort};
+	while (numsPointer < nums.length) {
+		output.push(nums[numsPointer]);
+		numsPointer++;
+	}
+	while (comparisonPointer < comparison.length) {
+		output.push(comparison[comparisonPointer]);
+		comparisonPointer++;
+	}
+
+	return output;
+}
+
+// the below code for mergeSort is taken from Springboard's solution; however, I am now fully aware how this code works together and better equipped to write recursive sorting algorithms in the future.
+function mergeSort(arr) {
+	if (arr.length <= 1) return arr;
+	const mid = Math.floor(arr.length / 2);
+	const left = mergeSort(arr.slice(0, mid));
+	const right = mergeSort(arr.slice(mid));
+
+	return merge(left, right);
+}
+
+module.exports = { merge, mergeSort };
